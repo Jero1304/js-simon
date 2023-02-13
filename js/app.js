0cd0389c2ea6698f2d1e6d6a4aca490e4b676d96
Jero1304 <calogeroleto13@gmail.com>
2023-02-13 15:33:50 +0100

    FEAT funzione confronta numeri

--- js/app.js
+++ js/app.js
@@ -5,19 +5,41 @@ let userNumber = [];
 
 randomNumber(pcNumber);
 userNum(userNumber);
+confronta(pcNumber, userNumber)
+
+
+
+
+
+
+
+
+
 
 
 function randomNumber(randomNum){
-    while (randomNum.length < 5 ) {
+    while (randomNum.length < 5) {
         let num = Math.floor(Math.random()* 100);
         randomNum.push(num);
     } 
-    console.log(randomNum);
+    // console.log(randomNum);
+    alert(randomNum);
 }
 
 function userNum(num){
     for (let i = 0; i < pcNumber.length; i++) {
-        num[i] = parseInt(prompt('inserisci',i,'numero'));
+        num[i] = parseInt(prompt('inserisci numero'));
+        while (isNaN(num[i] && num[i] < 101)) {
+        num[i] = parseInt(prompt('inserisci numero'));
+        }
         console.log(num); 
+    }
+}
+
+function confronta(pc , user){
+    for (let i = 0; i < pc.length; i++) {
+        if(pc.includes(user[i])){
+            console.log('elemento',user[i],'trovato');
+        }
     }
 }
